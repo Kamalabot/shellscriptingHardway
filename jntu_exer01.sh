c9b6bc865fcccad93abc43b49380a8b681766500
solverbot <kamaljp@gmail.com>
2022-12-12 15:42:38 +0530

    completed the exercise 1 and pushed

--- jntu_exer01.sh
+++ jntu_exer01.sh
@@ -1,10 +1,28 @@
-!#/usr/bin/env bash
+#!/usr/bin/bash
 
 #script takes a filename, starting and ending line provides the data between those lines
-read "Provide the filename, Starting Line and Ending line respectively: " file, start, end
+read -p "Provide the filename, Starting Line and Ending line respectively: " file start end
 
 #echo out the data provided by the user
+if [ -f $file ];
+#if file name is valid condition
+then
+  echo The filename is $file. Starting line is $start. Ending line is $end
+  fileline=$(wc -l $file|cut -d" " -f1)
+  #echo out the number of lines in the file
+  echo Total number of lines in the file is $fileline
+  #calculate the actual start required
+  actualstart=`expr $fileline - $start`
+  #echo out the actual start
+  echo The actual start is $actualstart
 
-echo The filename is $file. Starting line is $start. Ending line is $end
+  echo start of file data
+  echo
+  tail -n $actualstart $file | head -n $end
+#if file name is not valid 
+else
+  echo Enter file name that exists
+fi
 
-
+#Additional checks like the number of line requested, how far the ending from the 
+#file ending line can be checked and updated
