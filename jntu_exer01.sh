81bc8b78a92174576ef22b6896b367dc55a07a1d
solverbot <kamaljp@gmail.com>
2022-12-12 15:10:24 +0530

    starting jntu exercise

---
+++ jntu_exer01.sh
@@ -0,0 +1,10 @@
+!#/usr/bin/env bash
+
+#script takes a filename, starting and ending line provides the data between those lines
+read "Provide the filename, Starting Line and Ending line respectively: " file, start, end
+
+#echo out the data provided by the user
+
+echo The filename is $file. Starting line is $start. Ending line is $end
+
+
